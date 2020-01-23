330c0c3dd4e0a32fb2a67453b8bdc50a9ebf63fa
Yulun Yao <allen980123@gmail.com>
2020-01-23 15:02:14 -0600

    Revert "pooling.cc improvements (#4767)"
    This reverts commit 23ba37d4650236b57a2869fe966504810ea11101.

--- src/relay/op/nn/pooling.cc
+++ src/relay/op/nn/pooling.cc
@@ -134,7 +134,10 @@ bool Pool2DRel(const Array<Type>& types,
     return false;
   }
 
-  std::vector<IndexExpr> oshape(dshape.begin(), dshape.end());
+  std::vector<IndexExpr> oshape;
+  for (const auto& e : dshape) {
+    oshape.push_back(e);
+  }
 
   if (dshape[hidx].as<tir::AnyNode>()) {
     oshape[hidx] = dshape[hidx];
@@ -772,7 +775,10 @@ bool Pool1DRel(const Array<Type>& types,
     return false;
   }
 
-  std::vector<IndexExpr> oshape(dshape.begin(), dshape.end());
+  std::vector<IndexExpr> oshape;
+  for (const auto& e : dshape) {
+    oshape.push_back(e);
+  }
 
   if (dshape[widx].as<tir::AnyNode>()) {
     oshape[widx] = dshape[widx];
@@ -960,9 +966,12 @@ bool Pool3DRel(const Array<Type>& types,
     return false;
   }
 
-  std::vector<IndexExpr> oshape(dshape.begin(), dshape.end());
+  std::vector<IndexExpr> oshape;
+  for (const auto& e : dshape) {
+    oshape.push_back(e);
+  }
 
-  int idxes[3] = {didx, hidx, widx};
+  std::vector<int> idxes = {didx, hidx, widx};
   for (int i = 0; i < 3; i++) {
     int ii = idxes[i];
     if (dshape[ii].as<tir::AnyNode>()) {
